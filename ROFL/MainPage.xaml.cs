3182923f180eeb4fc1b63a45d68eaa8b05cc8575
Davidah121 <42045018+Davidah121@users.noreply.github.com>
2020-04-13 16:06:22 -0400

    Adding groups and transitions
    Can apply transforms to entire groups so this will likely be how we move to different screens in the app. Also, gotta fix the layout stuff.

--- ROFL/MainPage.xaml.cs
+++ ROFL/MainPage.xaml.cs
@@ -16,6 +16,8 @@ namespace ROFL
     {
         int count = 0;
         DetectShakeTest dt;
+        bool isGroup1 = true;
+        bool b2Pressed = true;
         public MainPage()
         {
             InitializeComponent();
@@ -37,6 +39,48 @@ namespace ROFL
                 Label1.TranslateTo(-100, 0, 500);
             else
                 Label1.TranslateTo(100, 0, 500);
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            if (b2Pressed == true)
+                Label2.Text = "This button doesn't do anything.";
+            else
+                Label2.Text = "THIS IS GROUP 2.";
+
+            b2Pressed = !b2Pressed;
+        }
+
+        private async void doAnimation()
+        {
+            Group1.IsVisible = true;
+            Group2.IsVisible = true;
+
+            if (isGroup1 == true)
+            {
+                await Task.WhenAll(
+                    Group1.TranslateTo(-500, 0, 500),
+                    Group2.TranslateTo(0, 0, 500)
+                );
+
+                Group1.IsVisible = false;
+            }
+            else
+            {
+                await Task.WhenAll(
+                    Group1.TranslateTo(0, 0, 500),
+                    Group2.TranslateTo(500, 0, 500)
+                );
+
+                Group2.IsVisible = false;
+            }
+
+            isGroup1 = !isGroup1;
+        }
+
+        private void transitionStuff(object sender, EventArgs e)
+        {
+            doAnimation();
         }
     }
 }
--- ROFL/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ ROFL/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,12 +17,24 @@ namespace ROFL {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.StackLayout Group1;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private global::Xamarin.Forms.Label Label1;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.StackLayout Group2;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label Label2;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            Group1 = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "Group1");
             Label1 = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "Label1");
+            Group2 = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "Group2");
+            Label2 = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "Label2");
         }
     }
 }
